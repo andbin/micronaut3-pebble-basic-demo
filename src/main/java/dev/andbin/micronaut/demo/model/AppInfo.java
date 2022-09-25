17d6f8987c9c313d3525ffa84db68be8ab919ec8
andbin <andbin@users.noreply.github.com>
2022-09-25 14:00:03 +0200

    Add initial project version for Micronaut 3.7.0

---
+++ src/main/java/dev/andbin/micronaut/demo/model/AppInfo.java
@@ -0,0 +1,60 @@
+/*
+SPDX-FileCopyrightText: Copyright (c) 2022 Andrea Binello ("andbin")
+SPDX-License-Identifier: MIT
+*/
+
+package dev.andbin.micronaut.demo.model;
+
+import java.util.Map;
+
+import io.micronaut.core.annotation.Introspected;
+
+@Introspected
+public class AppInfo {
+    private String javaRuntimeVersion;
+    private String micronautVersion;
+    private String pebbleVersion;
+    private Map<String, String> nettyVersions;
+
+    public String getJavaRuntimeVersion() {
+        return javaRuntimeVersion;
+    }
+
+    public void setJavaRuntimeVersion(String javaRuntimeVersion) {
+        this.javaRuntimeVersion = javaRuntimeVersion;
+    }
+
+    public String getMicronautVersion() {
+        return micronautVersion;
+    }
+
+    public void setMicronautVersion(String micronautVersion) {
+        this.micronautVersion = micronautVersion;
+    }
+
+    public String getPebbleVersion() {
+        return pebbleVersion;
+    }
+
+    public void setPebbleVersion(String pebbleVersion) {
+        this.pebbleVersion = pebbleVersion;
+    }
+
+    public Map<String, String> getNettyVersions() {
+        return nettyVersions;
+    }
+
+    public void setNettyVersions(Map<String, String> nettyVersions) {
+        this.nettyVersions = nettyVersions;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "["
+                + "javaRuntimeVersion=" + javaRuntimeVersion + ", "
+                + "micronautVersion=" + micronautVersion + ", "
+                + "pebbleVersion=" + pebbleVersion + ", "
+                + "nettyVersions=" + nettyVersions
+                + "]";
+    }
+}
---
+++ src/main/java/dev/andbin/micronaut/demo/service/AppInfoService.java
@@ -0,0 +1,61 @@
+/*
+SPDX-FileCopyrightText: Copyright (c) 2022 Andrea Binello ("andbin")
+SPDX-License-Identifier: MIT
+*/
+
+package dev.andbin.micronaut.demo.service;
+
+import java.util.Map;
+import java.util.Properties;
+import java.util.TreeMap;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import dev.andbin.micronaut.demo.model.AppInfo;
+import jakarta.inject.Singleton;
+
+@Singleton
+public class AppInfoService {
+    private static final Logger logger = LoggerFactory.getLogger(AppInfoService.class);
+
+    public AppInfo getAppInfo() {
+        AppInfo appInfo = new AppInfo();
+        appInfo.setJavaRuntimeVersion(getJavaRuntimeVersion());
+        appInfo.setMicronautVersion(getMicronautVersion());
+        appInfo.setPebbleVersion(getPebbleVersion());
+        appInfo.setNettyVersions(getNettyVersions());
+        return appInfo;
+    }
+
+    private String getJavaRuntimeVersion() {
+        return Runtime.version().toString();   // Java 9+
+    }
+
+    private String getMicronautVersion() {
+        return io.micronaut.core.version.VersionUtils.getMicronautVersion();
+    }
+
+    private String getPebbleVersion() {
+        // Note: this is the only way I found to get Pebble version ...
+        Properties props = new Properties();
+
+        try (var is = getClass().getResourceAsStream("/META-INF/maven/io.pebbletemplates/pebble/pom.properties")) {
+            props.load(is);
+        } catch (Exception e) {
+            logger.error("Failed loading of Pebble pom.properties", e);
+        }
+
+        return props.getProperty("version");
+    }
+
+    private Map<String, String> getNettyVersions() {
+        var versions = new TreeMap<String, String>();
+
+        for (var entry : io.netty.util.Version.identify().entrySet()) {
+            versions.put(entry.getKey(), entry.getValue().artifactVersion());
+        }
+
+        return versions;
+    }
+}
